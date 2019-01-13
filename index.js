77129832d910e47014c2de79e11d4b85276ffcb8
Anna Pawl <annampawl@gmail.com>
2019-01-12 17:39:00 -0800

    update index.js, added category model

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-const express = require('express')
-const index = express()
+// EXPRESS MODULES & OBJECTS
+const express = require('express');
+const index = express();
+// bodyParser
+// methodOverride
+// process.env.PORT || 3000
+
+// DATABASE
+
+// ROUTES
+// Import Toolbox Model
+// const Toolbox = require('./models/toolbox');
+// Import Category Model
+// const Category = require('./models/category');
+
+// HANDLEBARS MATERIAL
+var exphbs = require('express-handlebars');
+index.engine('handlebars', exphbs({defaultLayout: 'main' }));
+index.set('view engine', 'handlebars');
+
+// MIDDLEWARE, ROUTE CONFIGURATION
+// index.use(bodyParser.urlencoded({ extended: true }));
+// index.use(methodOverride('_method'))
 
 index.get('/', (req, res) => {
   res.render('home', { msg: 'Handlebars are Cool!' });
@@ -8,9 +29,3 @@ index.get('/', (req, res) => {
 index.listen(3000, () => {
   console.log('App listening on port 3000!')
 })
-
-// Handlebars Material
-var exphbs = require('express-handlebars');
-
-index.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-index.set('view engine', 'handlebars');
